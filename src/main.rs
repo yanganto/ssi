7a2f4e5a5c4edaca2a481787b160b14af48bb8fb
Antonio Yang <yanganto@gmail.com>
2020-06-28 10:46:13 +0800

    init rocksdb connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use array_bytes::fixed_hex_bytes_unchecked;
+use rocksdb::{IteratorMode, Options, DB};
+
 fn main() {
-    println!("Hello, world!");
+    let mut opts = Options::default();
+    let db = DB::open_cf_for_read_only(
+        &opts,
+        "./db",
+        vec![
+            "default", "col0", "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8",
+            "col9", "col10",
+        ],
+        false,
+    )
+    .unwrap();
+    // match db.get(fixed_hex_bytes_unchecked!(
+    //     // "0x09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd",
+    //     "0xb8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1",
+    //     32
+    // )) {
+    //     Ok(Some(value)) => println!("retrieved value {:?}", value),
+    //     Ok(None) => println!("value not found"),
+    //     Err(e) => println!("operational problem encountered: {}", e),
+    // }
+    let mut iter = db.iterator(IteratorMode::Start); // Always iterates forward
+    println!("{:?}", iter.next());
+    for (key, value) in iter {
+        println!("Saw {:?} {:?}", key, value);
+    }
+    iter = db.iterator(IteratorMode::End); // Always iterates backward
+    for (key, value) in iter {
+        println!("Saw {:?} {:?}", key, value);
+    }
 }
 
 //  CurrentIndex get(fn current_index): SessionIndex;
@@ -18,3 +49,6 @@ fn main() {
 //	DidUpdate: bool;
 //	"f0c365c3cf59d671eb72da0e7a4113c4bbd108c4899964f707fdaffb82636065|Timestamp|DidUpdate",
 //	 --------------------------------
+//
+//	 state root hash is "0x09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd"
+//	 extrinsics root hash is "0xb8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1"
