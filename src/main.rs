fb3a7d167dbc8e74f79aa1cfca6d895808c78898
Antonio Yang <yanganto@gmail.com>
2020-06-29 17:10:31 +0800

    decode node of state root

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[macro_use]
 use hex_literal::hex;
-use hash_db::{AsHashDB, HashDB, HashDBRef, Prefix};
+use hash_db::{AsHashDB, HashDB, HashDBRef, Hasher as HashDBHasher, Prefix};
 use rocksdb::{IteratorMode, Options, DB};
-use std::sync::Arc;
+use sp_std;
+use trie::node_codec::NodeCodec;
 use trie_db::{TrieDB, TrieDBNodeIterator, TrieLayout};
 
 pub mod blake2 {
@@ -14,17 +15,16 @@ pub mod blake2 {
     impl StdHasherTrait for StdHasher {
         #[inline]
         fn finish(&self) -> u64 {
-            unimplemented!();
+            unimplemented!("finish of StdHasherTrait unimplement");
         }
 
         #[inline]
         #[allow(unused_assignments)]
         fn write(&mut self, bytes: &[u8]) {
-            unimplemented!();
+            unimplemented!("write of StdHasherTrait unimplement");
         }
     }
 
-    /// Concrete implementation of Hasher using Blake2b 256-bit hashes
     #[derive(Debug)]
     pub struct Blake2Hasher;
 
@@ -34,7 +34,7 @@ pub mod blake2 {
         const LENGTH: usize = 32;
 
         fn hash(x: &[u8]) -> Self::Out {
-            unimplemented!();
+            unimplemented!("hash of Hasher unimplement");
         }
     }
 }
@@ -42,11 +42,12 @@ pub mod blake2 {
 type Hash = [u8; 32];
 type Hasher = crate::blake2::Blake2Hasher;
 
-pub struct SimpleTrie {
-    pub db: Arc<DB>,
+pub struct SimpleTrie<'a> {
+    pub db: DB,
+    pub cfs: Vec<&'a str>,
 }
 
-impl<'a> AsHashDB<Hasher, Vec<u8>> for SimpleTrie {
+impl<'a> AsHashDB<Hasher, Vec<u8>> for SimpleTrie<'a> {
     fn as_hash_db(&self) -> &dyn hash_db::HashDB<Hasher, Vec<u8>> {
         &*self
     }
@@ -56,78 +57,111 @@ impl<'a> AsHashDB<Hasher, Vec<u8>> for SimpleTrie {
     }
 }
 
-impl<'a> HashDB<Hasher, Vec<u8>> for SimpleTrie {
+impl<'a> HashDB<Hasher, Vec<u8>> for SimpleTrie<'a> {
     fn get(&self, key: &Hash, prefix: Prefix) -> Option<Vec<u8>> {
-        self.db.get(&key).expect("Database error")
+        // TODO: handle Prefix
+        for cf in self.cfs.clone() {
+            let h = self.db.cf_handle(cf).unwrap();
+            if let Some(v) = self.db.get_cf(h, &key).unwrap() {
+                return Some(v);
+            }
+        }
+        None
     }
 
     fn contains(&self, hash: &Hash, prefix: Prefix) -> bool {
-        //TODO:
-        // self.get(hash, prefix).is_some()
-        unimplemented!();
+        // TODO: handle Prefix
+        for cf in self.cfs.clone() {
+            let h = self.db.cf_handle(cf).unwrap();
+            if self.db.get_cf(h, &hash).unwrap().is_some() {
+                return true;
+            }
+        }
+        false
     }
 
     fn insert(&mut self, prefix: Prefix, value: &[u8]) -> Hash {
-        unimplemented!();
+        unimplemented!("insert of HashDB unimplement");
     }
 
     fn emplace(&mut self, key: Hash, prefix: Prefix, value: Vec<u8>) {
-        unimplemented!();
+        unimplemented!("emplace of HashDB unimplement");
     }
 
     fn remove(&mut self, key: &Hash, prefix: Prefix) {
-        unimplemented!();
+        unimplemented!("remove of HashDB unimplement");
     }
 }
 
-impl HashDBRef<Hasher, Vec<u8>> for SimpleTrie {
+impl<'a> HashDBRef<Hasher, Vec<u8>> for SimpleTrie<'a> {
     fn get(&self, key: &Hash, prefix: Prefix) -> Option<Vec<u8>> {
-        self.db.get(&key).expect("Database error")
+        // TODO: handle Prefix
+        for cf in self.cfs.clone() {
+            let h = self.db.cf_handle(cf).unwrap();
+            if let Some(v) = self.db.get_cf(h, &key).unwrap() {
+                return Some(v);
+            }
+        }
+        None
     }
 
     fn contains(&self, hash: &Hash, prefix: Prefix) -> bool {
-        //TODO:
-        // self.get(hash, prefix).is_some()
-        unimplemented!();
+        // TODO: handle Prefix
+        for cf in self.cfs.clone() {
+            let h = self.db.cf_handle(cf).unwrap();
+            if self.db.get_cf(h, &hash).unwrap().is_some() {
+                return true;
+            }
+        }
+        false
     }
 }
 
-// dyn hash_db::HashDBRef<_, std::vec::Vec<u8>>
-// impl HashDBRef<_, std::vec::Vec<u8>> for DB {}
-//  hash_db::HashDBRef<_, std::vec::Vec<u8>>`
+pub struct Layout<H>(sp_std::marker::PhantomData<H>);
+
+impl<H: HashDBHasher> TrieLayout for Layout<H> {
+    const USE_EXTENSION: bool = false;
+    type Hash = H;
+    type Codec = NodeCodec<Self::Hash>;
+}
 
 fn main() {
-    let mut opts = Options::default();
+    let opts = Options::default();
     let cfs = vec![
         "default", "col0", "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9",
         "col10",
     ];
     let db = DB::open_cf(&opts, "./db", cfs.clone()).unwrap();
 
-    let state_root = &hex!("09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd")[..];
-    let extrinsics_root =
-        &hex!("b8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1")[..];
+    let state_root = &hex!("09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd");
+    let extrinsics_root = &hex!("b8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1");
 
     for cf in cfs.iter() {
         let h = db.cf_handle(cf).unwrap();
         let iter = db.iterator_cf(h, IteratorMode::Start);
         for (key, value) in iter {
-            if *key == *state_root {
+            if *key == state_root[..] {
                 println!("Got state root hash in column family {}", cf);
                 println!("Saw {:?} \n {:?}", key, value);
             };
-            if *key == *extrinsics_root {
+            if *key == extrinsics_root[..] {
                 println!("Got extrinsic root hash in column family {}", cf);
                 println!("Saw {:?} \n {:?}", key, value);
             };
         }
     }
 
-    let simple_trie = SimpleTrie { db: Arc::new(db) };
+    let simple_trie = SimpleTrie { db, cfs };
 
-    // TODO Fix TrieLayout
-    // let trie = TrieDB::<_, TrieLayout>::new(&simple_trie, &state_root).unwrap();
-    // let mut iter = trie_db::TrieDBNodeIterator::new(&trie).unwrap();
+    let trie = TrieDB::<Layout<Hasher>>::new(&simple_trie, state_root).unwrap();
+    let mut iter = TrieDBNodeIterator::new(&trie).unwrap();
+    loop {
+        let item = iter.next();
+        if item.is_none() {
+            break;
+        }
+        println!("{:?}", item);
+    }
 }
 
 //  CurrentIndex get(fn current_index): SessionIndex;
