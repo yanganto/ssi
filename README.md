84b5e227f72d5c6436925d63b3848dbd30a9fe40
Antonio Yang <yanganto@gmail.com>
2020-07-07 11:33:21 +0800

    update state node decode finding

--- README.md
+++ README.md
@@ -7,31 +7,25 @@ Before tracing, there are articles explaning the keys in substrate.
 
 ## Dependency Analysis
 
+```
 | Modules                   | Who use this                                                             | Important Dependency               |
 |---------------------------|--------------------------------------------------------------------------|------------------------------------|
 | frame/support             | frames                                                                   | sp-statemachine                    |
-
-| Modules                   | Who use this                                                             | Important Dependency               |
 |---------------------------|--------------------------------------------------------------------------|------------------------------------|
 | primitives/state-manchine | frame/support                                                            | sp-core/storage(sp-storage),       |
 |                           |                                                                          | trie-db(parity), trie-root(parity) |
-
-| Modules                   | Who use this                                                             | Important Dependency               |
 |---------------------------|--------------------------------------------------------------------------|------------------------------------|
 | primitives/database       | client/db, client/api, utils/browser                                     | kvdb (parity-common)               |
-
-| Modules                   | Who use this                                                             | Important Dependency               |
 |---------------------------|--------------------------------------------------------------------------|------------------------------------|
 | primitives/storage        | client/service, client/api                                               | serde                              |
 |                           | frame/democracy, frame/staking, frame/transcation-payment, frame/vesting |                                    |
 |                           | primitives/externalities, primitives/core                                |                                    |
 |                           | utils/frame/rpc/support                                                  |                                    |
-
-| Modules                   | Who use this                                                             | Important Dependency               |
 |---------------------------|--------------------------------------------------------------------------|------------------------------------|
 | primitives/trie           | client/service, client/api, client/executor, client/block-builder        |                                    |
 |                           | frame/session                                                            |                                    |
 |                           | primitives/io, primitives/state-machine                                  |                                    |
+```
 
 
 ### Storage path
@@ -102,13 +96,75 @@ Before tracing, there are articles explaning the keys in substrate.
         NibbledBranch(NodeKey, Box<[Option<NodeHandle<H>>; 16]>, Option<DBValue>),
       }
     ```
+    ```rust
+      impl<D: Borrow<[u8]>> OwnedNode<D> {
+        /// Construct an `OwnedNode` by decoding an owned data source according to some codec.
+        pub fn new<C: NodeCodec>(data: D) -> Result<Self, C::Error> {
+          let plan = C::decode_plan(data.borrow())?;
+          Ok(OwnedNode { data, plan })
+        }
 
+        /// Returns a reference to the backing data.
+        pub fn data(&self) -> &[u8] {
+          self.data.borrow()
+        }
+
+        /// Returns a reference to the node decode plan.
+        pub fn node_plan(&self) -> &NodePlan {
+          &self.plan
+        }
+
+        /// Construct a `Node` by borrowing data from this struct.
+        pub fn node(&self) -> Node {
+          self.plan.build(self.data.borrow())
+        }
+      }
+    ```
+    ```rust
+    /// A reference to a trie node which may be stored within another trie node.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub enum NodeHandle<'a> {
+      Hash(&'a [u8]),
+      Inline(&'a [u8]),
+    }
+    ```
   - `trie-root` - a root calculated entirely in-memory
 
 ### hash of trie 
   - the hash root of extrinsic can be caluculate by `extrinsics_root` and `state_root` in system frame
   - the state trie root hash and extrinsics root hash are in block header
   - the block header is handled in the client modules
+  - the data structure use 0x80 as a seperator, which is invisible because it is not in ISO-IEC 5559 (ASCII) and not in ISO-IEC 5559-1 (EASCII) 
+
+  ```
+  // The raw data get from the state root hash
+  // 128 (0x80) is the seperator and contain 6 hash
+  // [
+  // 128,
+  // 44, 152,
+  // 128,
+  // 195, 162, 207, 123, 55, 18, 222, 77, 1, 22, 17, 199, 219, 213, 251, 6, 219, 238, 93, 205, 228, 57, 200, 241, 174, 67, 28, 169, 56, 67, 133, 227,
+  // 128,
+  // 163, 1, 165, 139, 133, 248, 193, 151, 249, 13, 162, 27, 86, 16, 212, 200, 254, 39, 224, 160, 190, 105, 225, 221, 19, 7, 246, 109, 6, 202, 107, 194,
+  // 128,
+  // 183, 207, 190, 0, 154, 221, 23, 122, 61, 23, 200, 77, 4, 206, 177, 67, 75, 70, 146, 247, 160, 60, 44, 1, 193, 112, 28, 200, 207, 30, 252, 13,
+  // 128,
+  // 169, 211, 94, 35, 16, 145, 205, 137, 159, 42, 39, 155, 8, 205, 23, 49, 194, 6, 0, 48, 123, 252, 28, 183, 111, 148, 14, 163, 184, 197, 136, 197,
+  // 128,
+  // 129, 101, 66, 162, 246, 165, 217, 92, 249, 6, 252, 250, 25, 216, 48, 38, 83, 144, 124, 58, 177, 32, 205, 61, 138, 219, 219, 23, 15, 86, 157, 170,
+  // 128,
+  // 14, 118, 239, 35, 99, 149, 106, 122, 22, 52, 150, 153, 107, 252, 84, 152, 196, 146, 67, 247, 231, 69, 60, 230, 252, 236, 175, 161, 113, 48, 23, 153
+  // ]
+  //
+  // Here is the decoded output, there are 6 child node have data
+  // [None, None, Hash, Hash, None, Hash, None, None, None, None, None, Hash, Hash, None, None, Hash]
+  // == child: Hash(4..36)       Hash([195, 162, 207, 123, 55, 18, 222, 77, 1, 22, 17, 199, 219, 213, 251, 6, 219, 238, 93, 205, 228, 57, 200, 241, 174, 67, 28, 169, 56, 67, 133, 227]),
+  // == child: Hash(37..69)      Hash([163, 1, 165, 139, 133, 248, 193, 151, 249, 13, 162, 27, 86, 16, 212, 200, 254, 39, 224, 160, 190, 105, 225, 221, 19, 7, 246, 109, 6, 202, 107, 194])
+  // == child: Hash(70..102)     Hash([183, 207, 190, 0, 154, 221, 23, 122, 61, 23, 200, 77, 4, 206, 177, 67, 75, 70, 146, 247, 160, 60, 44, 1, 193, 112, 28, 200, 207, 30, 252, 13])
+  // == child: Hash(103..135)    Hash([169, 211, 94, 35, 16, 145, 205, 137, 159, 42, 39, 155, 8, 205, 23, 49, 194, 6, 0, 48, 123, 252, 28, 183, 111, 148, 14, 163, 184, 197, 136, 197])
+  // == child: Hash(136..168)    Hash([129, 101, 66, 162, 246, 165, 217, 92, 249, 6, 252, 250, 25, 216, 48, 38, 83, 144, 124, 58, 177, 32, 205, 61, 138, 219, 219, 23, 15, 86, 157, 170])
+  // == child: Hash(169..201)    Hash([14, 118, 239, 35, 99, 149, 106, 122, 22, 52, 150, 153, 107, 252, 84, 152, 196, 146, 67, 247, 231, 69, 60, 230, 252, 236, 175, 161, 113, 48, 23, 153])
+  ```
 
 ### Conclusion
 To get things in the db and by pass the RPC, following parameters are needed:
@@ -125,7 +181,25 @@ Following is the RPC call with curl command to help you get the root hash.
 - ```curl -X POST -H "Content-Type: application/json"  --data '{"id":6,"jsonrpc":"2.0","method":"state_queryExtrinsicsRoot","params":[]}' 127.0.0.1:9933```
 
 # Column Family in RocksDB
-The state root hash is store as a key in Column Family `col1`.
+The state root hash is store as a key in Column Family `col1`. The column family meaning is shown as following code in substrate.
+
+```rust
+pub(crate) mod columns {
+	pub const META: u32 = crate::utils::COLUMN_META;
+	pub const STATE: u32 = 1;
+	pub const STATE_META: u32 = 2;
+	/// maps hashes to lookup keys and numbers to canon hashes.
+	pub const KEY_LOOKUP: u32 = 3;
+	pub const HEADER: u32 = 4;
+	pub const BODY: u32 = 5;
+	pub const JUSTIFICATION: u32 = 6;
+	pub const CHANGES_TRIE: u32 = 7;
+	pub const AUX: u32 = 8;
+	/// Offchain workers local storage
+	pub const OFFCHAIN: u32 = 9;
+	pub const CACHE: u32 = 10;
+}
+```
 
 ## DB Sample
 The db is generated by [substrate-node-template](https://github.com/yanganto/substrate-node-template/tree/expose-ext-root) with dev chain option. The state root hash is "0x09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd", and the extrinsics root hash is "0xb8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1".
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use colored::*;
 #[macro_use]
 use hex_literal::hex;
 use hash_db::{AsHashDB, HashDB, HashDBRef, Hasher as HashDBHasher, Prefix};
 use rocksdb::{IteratorMode, Options, DB};
 use sp_std;
 use trie::node_codec::NodeCodec;
-use trie_db::{TrieDB, TrieDBNodeIterator, TrieLayout};
+use trie_db::{
+    node::{Node, NodeHandle, NodePlan},
+    TrieDB, TrieDBNodeIterator, TrieLayout,
+};
 
 pub mod blake2 {
     use hash_db::Hasher;
@@ -96,6 +100,7 @@ impl<'a> HashDB<Hasher, Vec<u8>> for SimpleTrie<'a> {
 impl<'a> HashDBRef<Hasher, Vec<u8>> for SimpleTrie<'a> {
     fn get(&self, key: &Hash, prefix: Prefix) -> Option<Vec<u8>> {
         // TODO: handle Prefix
+        println!("get prefix: {:?} key({}): {:?}", prefix, key.len(), key);
         for cf in self.cfs.clone() {
             let h = self.db.cf_handle(cf).unwrap();
             if let Some(v) = self.db.get_cf(h, &key).unwrap() {
@@ -107,6 +112,12 @@ impl<'a> HashDBRef<Hasher, Vec<u8>> for SimpleTrie<'a> {
 
     fn contains(&self, hash: &Hash, prefix: Prefix) -> bool {
         // TODO: handle Prefix
+        println!(
+            "contains prefix: {:?} key({}): {:?}",
+            prefix,
+            hash.len(),
+            hash
+        );
         for cf in self.cfs.clone() {
             let h = self.db.cf_handle(cf).unwrap();
             if self.db.get_cf(h, &hash).unwrap().is_some() {
@@ -124,46 +135,160 @@ impl<H: HashDBHasher> TrieLayout for Layout<H> {
     type Hash = H;
     type Codec = NodeCodec<Self::Hash>;
 }
-
-fn main() {
+fn setup_db_connection() -> (DB, Vec<&'static str>) {
     let opts = Options::default();
     let cfs = vec![
         "default", "col0", "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9",
         "col10",
     ];
     let db = DB::open_cf(&opts, "./db", cfs.clone()).unwrap();
+    (db, cfs)
+}
 
+fn main() {
     let state_root = &hex!("09c0a468b841682c4cf29297408fadba23329fb7c0c5c81163c40f28f5caa5cd");
-    let extrinsics_root = &hex!("b8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1");
+    {
+        println!(
+            "{}",
+            "We have state root, and the db, let's pull this node out".cyan()
+        );
+        let (db, cfs) = setup_db_connection();
+        // [9, 192, 164, 104, 184, 65, 104, 44, 76, 242, 146, 151, 64, 143, 173, 186, 35, 50, 159, 183, 192, 197, 200, 17, 99, 196, 15, 40, 245, 202, 165, 205]
 
-    for cf in cfs.iter() {
-        let h = db.cf_handle(cf).unwrap();
-        let iter = db.iterator_cf(h, IteratorMode::Start);
-        for (key, value) in iter {
-            if *key == state_root[..] {
-                println!("Got state root hash in column family {}", cf);
-                println!("Saw {:?} \n {:?}", key, value);
-            };
-            if *key == extrinsics_root[..] {
-                println!("Got extrinsic root hash in column family {}", cf);
-                println!("Saw {:?} \n {:?}", key, value);
-            };
+        let extrinsics_root =
+            &hex!("b8946898950fe86044251bf4b9e0a71c0304d119fdee180ab6129059c698dbd1");
+
+        for cf in cfs.iter() {
+            let h = db.cf_handle(cf).unwrap();
+            let iter = db.iterator_cf(h, IteratorMode::Start);
+            for (key, value) in iter {
+                if *key == state_root[..] {
+                    println!("Got state root hash in column family {}", cf);
+                    println!("State Root {:?} \n {:?}", key, value);
+                };
+
+                // 128 is the seperator and contain 6 hash
+                // [
+                // 128,
+                // 44, 152,
+                // 128,
+                // 195, 162, 207, 123, 55, 18, 222, 77, 1, 22, 17, 199, 219, 213, 251, 6, 219, 238, 93, 205, 228, 57, 200, 241, 174, 67, 28, 169, 56, 67, 133, 227,
+                // 128,
+                // 163, 1, 165, 139, 133, 248, 193, 151, 249, 13, 162, 27, 86, 16, 212, 200, 254, 39, 224, 160, 190, 105, 225, 221, 19, 7, 246, 109, 6, 202, 107, 194,
+                // 128,
+                // 183, 207, 190, 0, 154, 221, 23, 122, 61, 23, 200, 77, 4, 206, 177, 67, 75, 70, 146, 247, 160, 60, 44, 1, 193, 112, 28, 200, 207, 30, 252, 13,
+                // 128,
+                // 169, 211, 94, 35, 16, 145, 205, 137, 159, 42, 39, 155, 8, 205, 23, 49, 194, 6, 0, 48, 123, 252, 28, 183, 111, 148, 14, 163, 184, 197, 136, 197,
+                // 128,
+                // 129, 101, 66, 162, 246, 165, 217, 92, 249, 6, 252, 250, 25, 216, 48, 38, 83, 144, 124, 58, 177, 32, 205, 61, 138, 219, 219, 23, 15, 86, 157, 170,
+                // 128,
+                // 14, 118, 239, 35, 99, 149, 106, 122, 22, 52, 150, 153, 107, 252, 84, 152, 196, 146, 67, 247, 231, 69, 60, 230, 252, 236, 175, 161, 113, 48, 23, 153
+                // ]
+
+                if *key == extrinsics_root[..] {
+                    println!("Got extrinsic root hash in column family {}", cf);
+                    println!("Extrinsic Root {:?} \n {:?}", key, value);
+                };
+            }
         }
     }
+    println!("==================================================================================");
+    {
+        println!("{}", "Decode the state node".cyan());
+        let (db, cfs) = setup_db_connection();
+        let simple_trie = SimpleTrie {
+            db,
+            cfs: cfs.clone(),
+        };
 
-    let simple_trie = SimpleTrie { db, cfs };
+        let trie = TrieDB::<Layout<Hasher>>::new(&simple_trie, state_root).unwrap();
+        let mut iter = TrieDBNodeIterator::new(&trie).unwrap();
 
-    let trie = TrieDB::<Layout<Hasher>>::new(&simple_trie, state_root).unwrap();
-    let mut iter = TrieDBNodeIterator::new(&trie).unwrap();
-    loop {
-        let item = iter.next();
-        if item.is_none() {
-            break;
+        let node = iter.next();
+        // Following code show the root node, and Hash structure use range to get the data as real hash
+        if let Some(n) = node {
+            let n = n.unwrap();
+            println!("= Nibble Vec: {:?}", n.0);
+            println!("= NodeKey: {:?}", n.1); // Exactly the Hash for the node
+            let owned_node = n.2.node();
+            println!("= owned_node: {:?}", owned_node);
+            let node_plan = n.2.node_plan();
+            match node_plan {
+                NodePlan::Branch { children, value } => {
+                    println!("== Branch");
+                    println!("== value: {:?}", value);
+                    for c in children {
+                        if let Some(h) = c {
+                            println!("== child: {:?}", h);
+                        }
+                    }
+                }
+                NodePlan::NibbledBranch {
+                    partial,
+                    children,
+                    value,
+                } => {
+                    println!("== NibbleBranch");
+                    println!("== partial: {:?}", partial);
+                    println!("== value: {:?}", value);
+                    for c in children {
+                        if let Some(h) = c {
+                            println!("== child: {:?}", h);
+                        }
+                    }
+                }
+                _ => {}
+            }
         }
-        println!("{:?}", item);
+        // Here is the decoded output, there are 6 child node have data
+        // [None, None, Hash, Hash, None, Hash, None, None, None, None, None, Hash, Hash, None, None, Hash]
+        // == child: Hash(4..36)		Hash([195, 162, 207, 123, 55, 18, 222, 77, 1, 22, 17, 199, 219, 213, 251, 6, 219, 238, 93, 205, 228, 57, 200, 241, 174, 67, 28, 169, 56, 67, 133, 227]),
+        //								c3a2cf7b3712de4d011611c7dbd5fb06dbee5dcde439c8f1ae431ca9384385e3
+        //
+        // == child: Hash(37..69)		Hash([163, 1, 165, 139, 133, 248, 193, 151, 249, 13, 162, 27, 86, 16, 212, 200, 254, 39, 224, 160, 190, 105, 225, 221, 19, 7, 246, 109, 6, 202, 107, 194])
+        //								a301a58b85f8c197f90da21b5610d4c8fe27e0a0be69e1dd1307f66d06ca6bc2
+        //
+        // == child: Hash(70..102)		Hash([183, 207, 190, 0, 154, 221, 23, 122, 61, 23, 200, 77, 4, 206, 177, 67, 75, 70, 146, 247, 160, 60, 44, 1, 193, 112, 28, 200, 207, 30, 252, 13])
+        //								b7cfbe009add177a3d17c84d04ceb1434b4692f7a03c2c01c1701cc8cf1efc0d
+        //
+        // == child: Hash(103..135)		Hash([169, 211, 94, 35, 16, 145, 205, 137, 159, 42, 39, 155, 8, 205, 23, 49, 194, 6, 0, 48, 123, 252, 28, 183, 111, 148, 14, 163, 184, 197, 136, 197])
+        //								a9d35e231091cd899f2a279b08cd1731c20600307bfc1cb76f940ea3b8c588c5
+        //
+        // == child: Hash(136..168)		Hash([129, 101, 66, 162, 246, 165, 217, 92, 249, 6, 252, 250, 25, 216, 48, 38, 83, 144, 124, 58, 177, 32, 205, 61, 138, 219, 219, 23, 15, 86, 157, 170])
+        //								816542a2f6a5d95cf906fcfa19d8302653907c3ab120cd3d8adbdb170f569daa
+        //
+        // == child: Hash(169..201)		Hash([14, 118, 239, 35, 99, 149, 106, 122, 22, 52, 150, 153, 107, 252, 84, 152, 196, 146, 67, 247, 231, 69, 60, 230, 252, 236, 175, 161, 113, 48, 23, 153])
+        //								0e76ef2363956a7a163496996bfc5498c49243f7e7453ce6fcecafa171301799
+    }
+    println!("==================================================================================");
+    {
+        println!("{}", "Find out the children".cyan());
+        let (db, cfs) = setup_db_connection();
+        let children = vec![
+            &hex!("c3a2cf7b3712de4d011611c7dbd5fb06dbee5dcde439c8f1ae431ca9384385e3"),
+            &hex!("a301a58b85f8c197f90da21b5610d4c8fe27e0a0be69e1dd1307f66d06ca6bc2"),
+            &hex!("b7cfbe009add177a3d17c84d04ceb1434b4692f7a03c2c01c1701cc8cf1efc0d"),
+            &hex!("a9d35e231091cd899f2a279b08cd1731c20600307bfc1cb76f940ea3b8c588c5"),
+            &hex!("816542a2f6a5d95cf906fcfa19d8302653907c3ab120cd3d8adbdb170f569daa"),
+            &hex!("0e76ef2363956a7a163496996bfc5498c49243f7e7453ce6fcecafa171301799"),
+        ];
+        for cf in cfs.iter() {
+            let h = db.cf_handle(cf).unwrap();
+            let iter = db.iterator_cf(h, IteratorMode::Start);
+            for (key, value) in iter {
+                for child in children.iter() {
+                    if *key == child[..] {
+                        println!("Got child hash in column family {}", cf);
+                        println!("child {:?} \n {:?}", key, value);
+                    };
+                }
+            }
+        }
     }
 }
 
+// Some materials to decode things from pallet
+//
 //  CurrentIndex get(fn current_index): SessionIndex;
 //	"cec5070d609dd3497f72bde07fc96ba072763800a36a99fdfc7c10f6415f6ee6|Session|CurrentIndex",
 //	 --------------------------------++++++++++++++++++++++++++++++++
